ebac096c8311032550f110cbe3a9e267616dbd09
ONGIDI E. GEOFREY <ongidigeofrey@gmail.com>
2024-06-11 17:10:17 +0300

    Fix: initializing db

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import ("github.com/gofiber/fiber/v2")
+import ("
+		github.com/gofiber/fiber/v2"
+		"codebrains.io/todolist/database"
+		"codebrains.io/todolist/models"
+		"gorm.io/driver/postgres"
+		"gorm.io/gorm"
+	)
 
 func helloWorld(c *fiber.Ctx) error{
 	return c.SendString("Welcome")
+}
+func initDatabase(){
+	
 }
 func main(){
 	app:=fiber.New();
